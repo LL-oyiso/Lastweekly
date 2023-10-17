a958479a70aa0e462e44220cf91545370536bc9f
LL-oyiso <theo.tose.tt@gmail.com>
2023-10-17 16:06:28 +0200

    nav bar changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 export const Navbar = () => {
   return (
     <div className="navbar navbar-sticky">
-	<div className="navbar-start">
-		<a className="navbar-item">News letter</a>
+	
 	</div>
 	<div className="navbar-end">
 		<a className="navbar-item">Home</a>
